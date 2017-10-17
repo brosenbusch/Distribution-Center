4ee84fdb2aa2a71e813ce971844fecd08d24b323
brosenbusch <8brosenbusch@stpatrick.org>
2017-10-17 14:43:47 -0500

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,32 @@
+const Product = function (){
+
+    let name = determineName();
+    let price = determineNumber();
+    let space = determineNumber();
+    let prime = determinePrime();
+
+    function determineName(){
+        let word = "";
+        for (let x=0; x<5; x++){
+            let c = Math.floor(25*Math.random()+65);
+            let letter = String.fromCharCode(c);
+            word += letter;
+        }
+        return word;
+    }
+
+    function determineNumber(){
+        let price = Math.round(100000*Math.random())/100;
+        return price;
+    }
+
+    function determinePrime(){
+        let num = Math.random()*100;
+        if (num > 50){
+            prime = true;
+        }
+        else{
+            prime = false;
+        }
+    }
+}
