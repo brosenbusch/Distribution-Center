a09f1401f3f9d368490da91e82969baac9bf1696
brosenbusch <8brosenbusch@stpatrick.org>
2017-10-24 14:24:41 -0500

    Add files via upload

--- DistributionCenter.js
+++ DistributionCenter.js
@@ -15,29 +15,43 @@ function configureWareHouse(numberofitems){
 function configureTruckFleet(numberoftrucks){
     let theFleet = new Queue();
     //fill the fleet with different sized trucks using math.random()
-    for(let x=0;x<numberofitems;x++){
-        theFleet.enqueue(new Truck();
+    for(let x=0;x<numberoftrucks;x++){
+        theFleet.enqueue(new Truck());
     }
     return theFleet;
 
 }
 function distribute(wh,tf){
     //distribute the products in the warehouse to the truck fleet.
-    
+    let ready = new Queue();
+    while(!wh.isEmpty() && !tf.isEmpty()){
+        let currentTruck = tf.dequeue();
+        while(currentTruck.spaceEfficiency()<1){
+            currentTruck.inventory.push(wh.dequeue());
+        }
+        if(currentTruck.spaceEfficiency()>1){
+            wh.enqueue(currentTruck.inventory.pop());
+        }
+        ready.enqueue(currentTruck);
+    }
+    return ready;
 }
-function ship(fleet,percent){
-    //if the trucks spaceEfficency is greater than or equal to the percent, then remove the truck from the fleet's linked list
-}
+
 function main(){
     let flemhouse = configureWareHouse();
     let flemfleet = configureTruckFleet();
-    flemfleet = distribute(flemhouse,flemfleet);
-    let shipping = ship(flemfleet,0.80);
-    for(let s=0;s<shipping.length;s++){
+    let ready = distribute(flemhouse,flemfleet);
+    //ready is empty
+    console.log(ready.dequeue());
+    for(let s=0;s<ready.length;s++){
         console.log("Truck "+s+" is in route");
         console.log("-----------------------");
-        while(s.getNext() != null){
-            console.log(shipping[s].getValue().spaceEfficiency());
+        while(!ready.isEmpty()){
+          let ct = ready.dequeue();
+          console.log(ct.spaceEfficiency());
+          for(let x=0; x<ct.inventory.length; x++){
+              console.log(ct.inventory[x].name);
+          }
         }
     }
 }
